cf4a808c04f6055d53522bd812cd14ab1cf7e5a5
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-05-10 19:09:15 +0000

    Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    i = 1
+    while i <= 100:
+        if (i % 3 == 0 and i % 5 == 0):
+            print("FIZZBUZZ ")
+        elif (i % 3 == 0):
+            print("Fizz ")
+        elif (i % 5 == 0):
+            print("Buzz ")
+        else:
+            print(i)
+        i += 1
