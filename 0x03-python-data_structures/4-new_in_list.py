56e08ab0c4040cc1299ae843e18b21b008518eef
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-05-13 10:02:51 -0700

    removed append in my function

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,10 @@
 #!urs/bin/python3
 def new_in_list(my_list, idx, element):
     i = 0
-    a = []
+    a = my_list[:]
     lenx = len(my_list)
     if (idx < 0 or idx > (lenx - 1)):
         return my_list
     else:
-        while i < lenx:
-            a.append(my_list[i])
-            i += 1
         a[idx] = element
         return a
