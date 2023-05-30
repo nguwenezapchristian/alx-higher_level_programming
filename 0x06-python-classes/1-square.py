a79082f5fd0c732152308635064b05edbb7686cc
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-05-30 12:32:47 -0700

    followed docstrings style

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 class Square:
-    """This is a second class
+    """
+    
+    This is a second class
     """
 
     def __init__(self, size):
-        """defines a square with a size
+        """
+        
+        defines a square with a size
+
 
         Args:
-            param1 (int): size of the square.
+            size (int): size of the square.
+
 
         """
+        
         self.__size = size
