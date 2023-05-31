8a39773982aafc19a35999c3fb57d293b0715d6a
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-05-31 07:29:04 -0700

    print matric

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
+    if len(matrix) == 0:
+        print()
+    elif matrix:
         lenx1 = len(matrix)
         lenx2 = len(matrix[0])
         for i in range(lenx1):
             for a in range(lenx2):
                 if a == lenx2 - 1:
-                    print(matrix[i][a])
+                    print("{}".format(matrix[i][a]))
                 else:
-                    print(matrix[i][a], end=' ')
-    else:
-        print()
+                    print("{}".format(matrix[i][a]), end=' ')
