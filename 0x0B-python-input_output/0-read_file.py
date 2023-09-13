65c13eaf1ae14f8a17f0287948189ab4a6cfd33b
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-09-13 21:50:11 +0200

    pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-""" 
-    
+"""
+
     a function that reads a text file (UTF8) and prints it to stdout
 
 """
+
+
 def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout
 
