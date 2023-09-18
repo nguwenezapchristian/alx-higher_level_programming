10bf56708363f0dd9ee00b34b9f7b52e68a32639
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-09-18 12:52:05 +0200

    This is the base for all classes in this project

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+
+
+    This is a Base class for all other
+    classes in this project.
+
+
+"""
+
+
+class Base():
+    """ this is the Base class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ contructor for the base class
+
+        Args:
+            id: id attribute
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
