a4715b7abf594eac6af609082f39eaa28c355b21
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-12-14 19:30:30 +0200

    user INNER JOIN

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,7 +2,7 @@
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
 -- You can use only one SELECT statement
-SELECT a.id AS id, a.name AS name, b.name AS state_name
+SELECT a.id AS id, a.name AS name, b.name AS name
 FROM cities a
 INNER JOIN states b ON a.state_id = b.id
 ORDER BY a.id ASC;
