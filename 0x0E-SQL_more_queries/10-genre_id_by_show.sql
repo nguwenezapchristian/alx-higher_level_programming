745b13449ebb1f3c37777bfc0188b80b7c396069
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-12-14 21:05:00 +0200

    a script that lists all shows contained in hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 -- that have at least one genre linked.
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM hbtn_0d_tvshows.tv_shows
 JOIN hbtn_0d_tvshows.tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
