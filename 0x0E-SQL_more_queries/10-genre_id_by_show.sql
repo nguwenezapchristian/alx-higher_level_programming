fbf1237c864440dcbd9721c209be440f3a61077d
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-12-14 21:10:00 +0200

    added ASC

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,4 +6,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM hbtn_0d_tvshows.tv_shows
 JOIN hbtn_0d_tvshows.tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
