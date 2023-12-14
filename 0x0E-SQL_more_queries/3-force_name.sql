99747de752f434d444184e470080630f8e476123
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-12-14 15:40:55 +0200

    added backticks on table name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,7 +1,7 @@
 -- a script that creates the table force_name on your MySQL server.
 -- id INT
 -- name VARCHAR(256) can’t be null
-CREATE TABLE IF NOT EXISTS force_name (
+CREATE TABLE IF NOT EXISTS `force_name` (
 	id INT,
 	name VARCHAR(256) TEXT NOT NULL
 	);
