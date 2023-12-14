dadaf7c6951a2d5a71c675bdde57e77c2ef6aaa9
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-12-14 14:09:36 +0200

    had to change GRANTS to GRANT

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- user_0d_1 should have all privileges on your MySQL server
 -- If the user user_0d_1 already exists, your script should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
