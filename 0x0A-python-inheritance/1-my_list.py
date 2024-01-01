5bb45412d78e91a4f97938c1b423ec21e023c1d6
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-01 16:54:34 +0200

    sort list in ascending order

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 """
- class MyList that inherits from list
+Module that inherits from list
 """
 
-
 class MyList(list):
-    def __init__(self):
-        self.list = list
+    """ MyList class which inherits from list """
+    def __init__(self, *args):
+        """ init as a constructor of MyList """
+        if args:
+            if len(args) != 1:
+                raise TypeError("Accepts one list argument")
+            if not isinstance(args[0], list):
+                raise TypeError("Must be a list")
+    
     def print_sorted(self):
-        for element in self:
-            if 
+        """ prints the list sorted in ascending """
+        sortedList = self.copy()
+
+        for i in range(len(sortedList)):
+            for j in range(i + 1, len(sortedList)):
+                if sortedList[i] > sortedList[j]:
+                    sortedList[i], sortedList[j] = sortedList[j], sortedList[i]
+        print(sortedList)
