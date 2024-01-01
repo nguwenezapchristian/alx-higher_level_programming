8d72d26f03724a83d2bee0fc1db3b11811743549
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-01 18:59:06 +0200

    removed trailing whitespace

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-returns True if the object is an instance of a class that inherited 
+returns True if the object is an instance of a class that inherited
 (directly or indirectly) from the specified class ; otherwise False
 """
 
