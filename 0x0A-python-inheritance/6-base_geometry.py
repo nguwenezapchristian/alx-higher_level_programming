77661acd0700584b37f6eaae8461a8afb081bbd4
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-01 18:22:54 +0200

    method which raise an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This an empty Module
+"""
+
+
+class BaseGeometry:
+    """ an empty class """
+    def __init__(self):
+        """ init as a constructor of a BaseGeometry """
+        pass
+
+    def area(self):
+        """ method area which raises an exception """
+        raise Exception("area() is not implemented)
