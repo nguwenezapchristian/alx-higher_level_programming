55dfffff3e4b45f8a1cdd0e16b84be7605bd4d4a
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-02 20:04:54 +0200

    using INNER JOIN

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,7 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 SELECT tv_shows.`title`, tv_show_genres.`genre_id`
-FROM hbtn_0d_tvshows.`tv_shows`
-JOIN hbtn_0d_tvshows.`tv_show_genres`
+FROM tv_shows
+INNER JOIN tv_show_genres
 ON tv_shows.`id` = tv_show_genres.`show_id`
 ORDER BY tv_shows.`title`, tv_show_genres.`genre_id`;
