81224f7010adc7ce31b1d7560a22b1ad734fcb0e
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-09 23:13:24 +0200

    edit the string

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 // a script that prints a message depending of the number of arguments passed
 const Args = process.argv.length;
 if (Args < 3) {
-  console.log('No arguments');
+  console.log('No argument');
 } else if (Args === 3) {
   console.log('Argument found');
 } else {
