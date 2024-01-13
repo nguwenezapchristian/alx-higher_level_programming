6cd63fd17d48735cfd52e36b1ef06a36b688931c
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-13 11:29:20 +0200

    pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,20 +6,27 @@ using the module MySQLdb
 import MySQLdb
 import sys
 
-try:
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-except MySQLdb.Error as e:
+if __name__ == "__main__":
     try:
-        print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
-    except IndexError:
-        print("MySQL Error: %s" % str(e))
-finally:
-    if 'db' in locals() and db is not None:
-        db.close()
+        db = MySQLdb.connect(
+                host='localhost',
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3],
+                port=3306
+                )
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id")
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: %s" % str(e))
+    finally:
+        if 'db' in locals() and db is not None:
+            db.close()
