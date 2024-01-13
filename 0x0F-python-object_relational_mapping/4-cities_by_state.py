2c6bbd301957470533bdd310ee33ac5e330603e0
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-13 20:12:03 +0200

    join two tables where states id is the same

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(
+                host='localhost',
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3],
+                port=3306
+                )
+        cur = db.cursor()
+        cur.execute("SELECT a.id AS id, a.name AS name, b.name AS name "
+                    "FROM cities a "
+                    "INNER JOIN states b ON a.state_id = b.id "
+                    "ORDER BY a.id ASC")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error: [%d] - [%s]" % (e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: [%s]" % str(e))
+    finally:
+        if 'cur' in locals() and cur is not None:
+            cur.close()
+        if 'db' in locals() and db is not None:
+            db.close()
