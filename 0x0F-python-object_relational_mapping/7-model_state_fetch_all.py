070f4ae2590fd544f9615313ea540a13a6d17800
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-14 21:45:27 +0200

    does not execute when imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,15 +8,20 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-username = argv[1]
-password = argv[2]
-database = argv[3]
-host = "localhost"
-connection = f'mysql+mysqldb://{username}:{password}@{host}:3306/{database}'
-engine = create_engine(connection)
-Session = sessionmaker(bind=engine)
-session_1 = Session()
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    host = "localhost"
+    connection = f'mysql+mysqldb://{username}:{password}@{host}:3306/{database}'
+    engine = create_engine(connection)
+    Session = sessionmaker(bind=engine)
+    session_1 = Session()
 
-states = session_1.query(State).order_by(State.id.asc()).all()
-for state in states:
-    print(state.id, state.name)
+    states = session_1.query(State).order_by(State.id.asc()).all()
+    for state in states:
+        print(state.id, state.name)
