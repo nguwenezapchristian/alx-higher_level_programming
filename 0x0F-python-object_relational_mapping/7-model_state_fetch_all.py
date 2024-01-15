edd9e8ff34fd17f131fe3a37b2d9576a2b80ecc8
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-15 20:26:01 +0200

    lists all data in table

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,7 +3,7 @@
 a script that lists all State objects
 from the database hbtn_0e_6_usa
 """
-from sqlalchemy import Column, Integer, String, create_engine, MetaData
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
@@ -17,12 +17,12 @@ if __name__ == "__main__":
     password = argv[2]
     database = argv[3]
     host = "localhost"
-    connection = f'mysql+mysqldb://\
-                    {username}:{password}@{host}:3306/{database}'
+    connection = f'mysql+mysqldb://{username}:\
+                {password}@{host}:3306/{database}'
     engine = create_engine(connection)
     Session = sessionmaker(bind=engine)
     session_1 = Session()
 
-    states = session_1.query(State).order_by(State.id.asc()).all()
+    states = session_1.query(State).order_by((State.id).asc()).all()
     for state in states:
-        print('{0}: {1}'.format(state.id, state.name))
+        print('{}: {}'.format(state.id, state.name))
