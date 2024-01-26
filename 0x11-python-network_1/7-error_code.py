910139a14fc49a3af52f37df0e45960d0abbb002
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-26 22:13:23 +0200

    raise in requests

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
         resp = requests.get(sys.argv[1])
         print(resp.text)
     except requests.exception.RequestException as error:
-        print(f"Error code: {error.status}")
+        if error.status >= 400:
+            print(f"Error code: {error.status}")
