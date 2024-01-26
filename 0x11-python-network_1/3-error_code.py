7b35935c56bce99f4c2a37c7e2d5b4798a713521
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-26 19:51:15 +0200

    forgot to import urllib.error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,6 +4,7 @@ a Python script that takes in a URL, sends a request to the
 URL and displays the body of the response (decoded in utf-8).
 """
 import urllib.request
+import urllib.error
 import sys
 
 
