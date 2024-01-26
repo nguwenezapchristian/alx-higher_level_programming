74e32a7523611d6abb89a024974ad40fe746e260
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-26 17:25:32 +0200

    retrieve X-Request-Id variable

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request to the URL and
+displays the value of the X-Request-Id variable found in the header
+of the response.
+"""
+import urllib.request
+import sys
+
+
+req = urllib.request.Request(sys.argv[1])
+with urllib.request.urlopen(req) as response:
+    headers = response.getheaders()
+
+for header in headers:
+    if header[0] == 'X-Request-Id':
+        print(header[1])
