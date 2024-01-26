e76c3d0d195ef6c78cba993c168d6772efb8f271
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-26 22:09:32 +0200

    raise in requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request to the
+URL and displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        resp = requests.get(sys.argv[1])
+        print(resp.text)
+    except requests.exception.RequestException as error:
+        print(f"Error code: {error.status}")
