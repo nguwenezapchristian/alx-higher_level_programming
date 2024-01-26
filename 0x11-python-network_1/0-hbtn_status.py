45a56dc623097f9b519090b3d121136ccc84c450
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-26 17:36:59 +0200

    when imported does not execute

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,12 @@ a Python script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
 
-print("Body response:")
-print(f"\t- type: {type(the_page)}")
-print(f"\t- content: {the_page}")
-print(f"\t- utf8 content: {the_page.decode('utf-8')}")
+    print("Body response:")
+    print(f"\t- type: {type(the_page)}")
+    print(f"\t- content: {the_page}")
+    print(f"\t- utf8 content: {the_page.decode('utf-8')}")
