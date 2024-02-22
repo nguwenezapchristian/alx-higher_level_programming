f88029f24d21ed04e805a753c9bce168b87bc9a2
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-02-22 14:11:05 +0200

    fixed semistandard error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-const { chownSync } = require('fs');
-
 // a script that reads and prints the content of a file.
 
 const fs = require('fs').promises;
@@ -11,15 +8,7 @@ async function readFile (filepath, encoding) {
     const data = await fs.readFile(filepath, encoding);
     console.log(data);
   } catch (error) {
-    // const errorObject = {
-    //     'Error': error.message,
-    //     'errno': error.errno,
-    //     'code': error.code,
-    //     'syscall': error.syscall,
-    //     'path': error.path
-    // }
     console.log(error);
-    // console.error(errorObject);
   }
 }
 readFile(process.argv[2], 'utf8');
