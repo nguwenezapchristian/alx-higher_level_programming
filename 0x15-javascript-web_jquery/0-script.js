6e3e8dc11d9afd706ada4e0cc5ded72697053055
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-02-28 21:26:00 +0200

    [UPDATE]

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 //  a JavaScript script that updates the text color
 // of the <header> element to red (#FF0000)
-let header = document.querySelector('header');
+const header = document.querySelector('header');
 header.style.color = '#FF0000';
--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // JavaScript script that updates the text color of
 // the <header> element to red (#FF0000)
-let $header = $('header');
+const $header = $('header');
 $header.css('color', '#FF0000');
